45dc2f1e0753fa8a03b8f6cb01cefe5cb54eb377
Minseok Choi <minchoidev@gmail.com>
2019-02-25 16:30:59 -0500

    Login, Register, Logout methods skeletons added

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,4 +23,29 @@ router.get('/', (req, res, next) => {
    });
 });
 
+/* Get - render login page */
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+
+/* POST - try login by input information */
+router.post('/login', (req, res, next) => {
+  res.render('content/index');
+});
+
+/* Get - render register page */
+router.get('/register', (req, res, next) => {
+  res.render('auth/register');
+});
+
+/* POST - process the information passed from the form and register new user */
+router.post('/register', (req, res, next) => {
+  res.render('content/index');
+});
+
+/* Get - logout the current user  */
+router.get('/logout', (req, res, next) => {
+  res.render('content/index');
+});
+
 module.exports = router;
